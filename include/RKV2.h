4f9211fd222fc18080664e51fb2ef293a292e9f4
tanoshiikoushi <68407204+tanoshiikoushi@users.noreply.github.com>
2021-07-18 21:19:31 -0600

    src/CMakeLists.txt

--- include/RKV2.h
+++ include/RKV2.h
@@ -1,7 +1,8 @@
 #ifndef RKV2_H_INCLUDED
 
-#define RKV2_H_INCLUDED
+const u8 data[] = { 'h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd'};
+u32 test_func();
 
-void test_func();
+#define RKV2_H_INCLUDED
 
 #endif // RKV2_H_INCLUDED
--- src/RKV2.cpp
+++ src/RKV2.cpp
@@ -1,5 +1,6 @@
+#include "checksum/crc32eth.h"
 #include "RKV2.h"
 
-void test_func() {
-	return;
+u32 test_func() {
+	return crc32eth(data, sizeof(data));
 }
