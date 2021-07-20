fa0347b762b53448d7de96c84cd0aeb550c43bd9
tanoshiikoushi <68407204+tanoshiikoushi@users.noreply.github.com>
2021-07-19 21:52:33 -0600

    implemented structures and partway through implementing load

--- include/RKV2.h
+++ include/RKV2.h
@@ -2,8 +2,55 @@
 
 #include "types/typedefs.h"
 
-const u8 data[] = { 'h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd'};
-u32 test_func();
+class RKV2File {
+    // these are private since you should not interface directly with the underlying structure I use
+    private:
+        u8* data;
+        u64 data_size;
+
+        u32 entry_count;
+        u32 entry_name_string_length; // one big string that is properly zero terminated -- libKoushiCommon will contain an amortized O(1) loader for this
+        u32 filepath_addendum_count;
+        u32 filepath_addendum_string_length; // same as above
+        u32 metadata_table_offset;
+        u32 metadata_table_length; // we don't really use this, but it doesn't hurt to track so we can update it properly later
+
+        RKV2Entry* entries;
+        RKV2FilePathAddendum* addendums;
+
+    // these are public since you should use the functions I provide
+    public:
+        // functions that I will need
+        bool load(const u8* buf_to_copy, const u32 buf_size);
+        bool extract(const u8* output_path); // this is going to be temporary and will use std::fstream, but I really do need to clean it up x.x
+        // load_filesystem();
+        // repack();
+        // obviously more to come
+
+    ~RKV2File() {
+        delete data;
+        delete[] entries;
+        delete[] addendums;
+    };
+};
+
+class RKV2Entry {
+    // these are public so that you can interact with the underlying entires if desired
+    public:
+        u32 entry_name_string_offset;
+        u32 padding; // could be an entry/addendum relationship according to Pixel, but seems to be unused from what I can tell
+        u32 entry_size;
+        u32 entry_offset;
+        u32 entry_crc32eth;
+};
+
+class RKV2FilePathAddendum {
+    // these are public so that you can interact with the underlying addendums if desired
+    public:
+        u64 filepath_addendum_string_offset;
+        u32 timestamp;
+        u32 entry_name_string_offset; // this ties the addendums to the entry
+};
 
 #define RKV2_H_INCLUDED
 
--- src/RKV2.cpp
+++ src/RKV2.cpp
@@ -1,6 +1,35 @@
-#include "checksum/crc32eth.h"
+#include <cstring>
+#include "rw_utilities/mem_read_utilities.h"
 #include "RKV2.h"
 
-u32 test_func() {
-	return crc32eth(data, sizeof(data));
+bool RKV2File::load(const u8* buf_to_copy, const u64 buf_size) {
+    // copy into our internal buffer
+    this.data = new u8[buf_size];
+    strncpy(this.data, buf_to_copy, buf_size);
+
+    // header check
+    if (read_32BE(this.data) != 0x524B5632) { // "RKV2"
+        delete this.data;
+        return false;
+    }
+
+    // read in the following useful info
+    this.entry_count = read_32LE(&this.data[0x04]);
+    this.entry_name_string_length = read_32LE(&this.data[0x08]);
+    this.filepath_addendum_count = read_32LE(&this.data[0x0C]);
+    this.filepath_addendum_string_length = read_32LE(&this.data[0x10]);
+    this.metadata_table_offset = read_32LE(&this.data[0x14]);
+    this.metadata_table_length = read_32LE(&this.data[0x18]);
+
+    // allocate buffers for our entries and addendums
+    this.entries = new RKV2Entry[this.entry_count];
+    this.addendums = new RKV2FilePathAddendum[this.filepath_addendum_count];
+
+    // now process these entries and addendums using our string_buf_readers :D
+
+    return true;
+}
+
+bool RKV2File::extract(const u8* output_path) {
+
 }
