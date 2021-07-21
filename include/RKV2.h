94de60381c1d902e8e03b93a39c83761e44c0551
tanoshiikoushi <68407204+tanoshiikoushi@users.noreply.github.com>
2021-07-20 20:10:01 -0600

    initial load function implementation for testing

--- include/RKV2.h
+++ include/RKV2.h
@@ -2,6 +2,31 @@
 
 #include "types/typedefs.h"
 
+// constants for string buffer sizes
+const u64 RKV2ENTRY_SIZE = 0x18;
+const u64 RKV2FILEPATHADDENDUM_SIZE = 0x10;
+
+const u64 BASE_ENTRY_STRING_SIZE = 0x16;
+const u64 BASE_FILEPATHADDENDUM_STRING_SIZE = 0x30;
+
+class RKV2Entry {
+    // these are public so that you can interact with the underlying entires if desired
+    public:
+        u32 entry_name_string_offset;
+        u32 padding; // could be an entry/addendum relationship according to Pixel, but seems to be unused from what I can tell
+        u32 entry_size;
+        u32 entry_offset;
+        u32 entry_crc32eth;
+};
+
+class RKV2FilePathAddendum {
+    // these are public so that you can interact with the underlying addendums if desired
+    public:
+        u64 filepath_addendum_string_offset;
+        u32 timestamp;
+        u32 entry_name_string_offset; // this ties the addendums to the entry
+};
+
 class RKV2File {
     // these are private since you should not interface directly with the underlying structure I use
     private:
@@ -21,35 +46,25 @@ class RKV2File {
     // these are public since you should use the functions I provide
     public:
         // functions that I will need
-        bool load(const u8* buf_to_copy, const u32 buf_size);
+        bool load(const u8* buf_to_copy, const u64 buf_size);
         bool extract(const u8* output_path); // this is going to be temporary and will use std::fstream, but I really do need to clean it up x.x
         // load_filesystem();
         // repack();
         // obviously more to come
 
     ~RKV2File() {
-        delete data;
-        delete[] entries;
-        delete[] addendums;
+        if (data) {
+            delete data;
+        }
+
+        if (entries) {
+            delete[] entries;
+        }
+
+        if (addendums) {
+            delete[] addendums;
+        }
     };
-};
-
-class RKV2Entry {
-    // these are public so that you can interact with the underlying entires if desired
-    public:
-        u32 entry_name_string_offset;
-        u32 padding; // could be an entry/addendum relationship according to Pixel, but seems to be unused from what I can tell
-        u32 entry_size;
-        u32 entry_offset;
-        u32 entry_crc32eth;
-};
-
-class RKV2FilePathAddendum {
-    // these are public so that you can interact with the underlying addendums if desired
-    public:
-        u64 filepath_addendum_string_offset;
-        u32 timestamp;
-        u32 entry_name_string_offset; // this ties the addendums to the entry
 };
 
 #define RKV2_H_INCLUDED
--- src/RKV2.cpp
+++ src/RKV2.cpp
@@ -1,35 +1,74 @@
 #include <cstring>
+#include <stdio.h>
+#include "string_utilities/string_buf_readers.h"
 #include "rw_utilities/mem_read_utilities.h"
 #include "RKV2.h"
 
 bool RKV2File::load(const u8* buf_to_copy, const u64 buf_size) {
     // copy into our internal buffer
-    this.data = new u8[buf_size];
-    strncpy(this.data, buf_to_copy, buf_size);
+    this->data = new u8[buf_size];
+    strncpy((char*)this->data, (char*)buf_to_copy, buf_size);
 
     // header check
-    if (read_32BE(this.data) != 0x524B5632) { // "RKV2"
-        delete this.data;
+    if (read_32BE(this->data) != 0x524B5632) { // "RKV2"
+        delete this->data;
         return false;
     }
 
     // read in the following useful info
-    this.entry_count = read_32LE(&this.data[0x04]);
-    this.entry_name_string_length = read_32LE(&this.data[0x08]);
-    this.filepath_addendum_count = read_32LE(&this.data[0x0C]);
-    this.filepath_addendum_string_length = read_32LE(&this.data[0x10]);
-    this.metadata_table_offset = read_32LE(&this.data[0x14]);
-    this.metadata_table_length = read_32LE(&this.data[0x18]);
+    this->entry_count = read_32LE(&this->data[0x04]);
+    this->entry_name_string_length = read_32LE(&this->data[0x08]);
+    this->filepath_addendum_count = read_32LE(&this->data[0x0C]);
+    this->filepath_addendum_string_length = read_32LE(&this->data[0x10]);
+    this->metadata_table_offset = read_32LE(&this->data[0x14]);
+    this->metadata_table_length = read_32LE(&this->data[0x18]);
 
     // allocate buffers for our entries and addendums
-    this.entries = new RKV2Entry[this.entry_count];
-    this.addendums = new RKV2FilePathAddendum[this.filepath_addendum_count];
+    this->entries = new RKV2Entry[this->entry_count];
+    this->addendums = new RKV2FilePathAddendum[this->filepath_addendum_count];
 
-    // now process these entries and addendums using our string_buf_readers :D
+    // now read in the entries
+    u64 curr_file_pos = this->metadata_table_offset;
+
+    u64 name_string_pos = this->metadata_table_offset + (RKV2ENTRY_SIZE * this->entry_count);
+
+    for (u32 i = 0; i < this->entry_count; i++) {
+        this->entries[i].entry_name_string_offset = read_32LE(&this->data[curr_file_pos]);
+        // next 4 bytes are padding
+        this->entries[i].entry_size = read_32LE(&this->data[curr_file_pos + 0x8]);
+        this->entries[i].entry_offset = read_32LE(&this->data[curr_file_pos + 0xC]);
+        this->entries[i].entry_crc32eth = read_32LE(&this->data[curr_file_pos + 0x10]);
+
+        curr_file_pos += RKV2ENTRY_SIZE;
+
+        u8* name = read_dynamic_string(&this->data[name_string_pos + this->entries[i].entry_name_string_offset], BASE_ENTRY_STRING_SIZE);
+        printf("Name: %s - String Offset: %->4X - Entry Offset: %->4X\n", name, this->entries[i].entry_name_string_offset, this->entries[i].entry_offset);
+        delete name;
+    }
+
+    // skip over the size of the entry name string
+    curr_file_pos += this->entry_name_string_length;
+
+    u64 addendum_name_string_pos = curr_file_pos + (RKV2FILEPATHADDENDUM_SIZE * this->filepath_addendum_count);
+
+    // now read in the addendums
+    for (u32 j = 0; j < this->filepath_addendum_count; j++) {
+        this->addendums[j].filepath_addendum_string_offset = read_64LE(&this->data[curr_file_pos]);
+        this->addendums[j].timestamp = read_32LE(&this->data[curr_file_pos + 0x8]);
+        this->addendums[j].entry_name_string_offset = read_32LE(&this->data[curr_file_pos + 0xC]);
+
+        curr_file_pos += RKV2FILEPATHADDENDUM_SIZE;
+
+        u8* filepath_name = read_dynamic_string(&this->data[addendum_name_string_pos + this->addendums[j].filepath_addendum_string_offset], BASE_FILEPATHADDENDUM_STRING_SIZE);
+        u8* name = read_dynamic_string(&this->data[addendum_name_string_pos + this->addendums[j].entry_name_string_offset], BASE_ENTRY_STRING_SIZE);
+        printf("Addendum Name: %s - String Offset: %->8X - Linked Name: %->4X\n", filepath_name, this->addendums[j].filepath_addendum_string_offset, name);
+        delete filepath_name;
+        delete name;
+    }
 
     return true;
 }
 
 bool RKV2File::extract(const u8* output_path) {
-
+    return false;
 }
