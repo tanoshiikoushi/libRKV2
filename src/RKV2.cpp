22ef0e441b8705902ef233e5315bb009e16d9b36
tanoshiikoushi <68407204+tanoshiikoushi@users.noreply.github.com>
2021-07-21 21:07:42 -0600

    extra debug logging info

--- src/RKV2.cpp
+++ src/RKV2.cpp
@@ -1,3 +1,4 @@
+#include <fstream>
 #include <cstring>
 #include <stdio.h>
 #include "string_utilities/string_buf_readers.h"
@@ -5,18 +6,23 @@
 #include "RKV2.h"
 
 bool RKV2File::load(const u8* buf_to_copy, const u64 buf_size) {
+    std::fstream log_file;
+    log_file.open("D:\\Ty 2 RE\\projs\\libRKV2\\log.txt", std::ios_base::out);
+    char* out_buf = new char[0x100];
+    u32 out_size = 0;
+
     // copy into our internal buffer
     this->data = new u8[buf_size];
     memcpy(this->data, buf_to_copy, buf_size);
-    printf("past copy\n");
+    log_file << "past copy\n";
 
     // header check
     if (read_32BE(this->data) != 0x524B5632) { // "RKV2"
-        printf("header bad");
+        log_file << "header bad";
         delete this->data;
         return false;
     }
-    printf("header good\n");
+    log_file << "header good\n";
 
     // read in the following useful info
     this->entry_count = read_32LE(&this->data[0x04]);
@@ -25,57 +31,83 @@ bool RKV2File::load(const u8* buf_to_copy, const u64 buf_size) {
     this->filepath_addendum_string_length = read_32LE(&this->data[0x10]);
     this->metadata_table_offset = read_32LE(&this->data[0x14]);
     this->metadata_table_length = read_32LE(&this->data[0x18]);
-    printf("metadata read\n");
+    log_file << "metadata read\n";
 
     // allocate buffers for our entries and addendums
     this->entries = new RKV2Entry[this->entry_count];
     this->addendums = new RKV2FilePathAddendum[this->filepath_addendum_count];
-    printf("buffers alloced\n");
+    log_file << "buffers alloced\n";
 
     // now read in the entries
     u64 curr_file_pos = this->metadata_table_offset;
 
     u64 name_string_pos = this->metadata_table_offset + (RKV2ENTRY_SIZE * this->entry_count);
 
-    printf("entering entry cycle with curr_file_pos: 0x%.8X - name_string_pos: 0x%.8X\n", curr_file_pos, name_string_pos);
+    out_size = snprintf(out_buf, 0x100, "entering entry cycle with curr_file_pos: 0x%.8X - name_string_pos: 0x%.8X\n", curr_file_pos, name_string_pos);
+    log_file.write(out_buf, out_size);
+
     for (u32 i = 0; i < this->entry_count; i++) {
+        out_size = snprintf(out_buf, 0x100, "pre-entry at 0x%.8X\n", curr_file_pos);
+        log_file.write(out_buf, out_size);
+
         this->entries[i].entry_name_string_offset = read_32LE(&this->data[curr_file_pos]);
         // next 4 bytes are padding
         this->entries[i].entry_size = read_32LE(&this->data[curr_file_pos + 0x8]);
         this->entries[i].entry_offset = read_32LE(&this->data[curr_file_pos + 0xC]);
         this->entries[i].entry_crc32eth = read_32LE(&this->data[curr_file_pos + 0x10]);
 
+        out_size = snprintf(out_buf, 0x100, "entry with details - str-off: 0x%.8X - size: 0x%.8X - offset: 0x%.8X - crc: 0x%.8X\n", this->entries[i].entry_name_string_offset,
+                            this->entries[i].entry_size, this->entries[i].entry_offset, this->entries[i].entry_crc32eth);
+        log_file.write(out_buf, out_size);
+
         curr_file_pos += RKV2ENTRY_SIZE;
 
         u8* name = read_dynamic_string(&this->data[name_string_pos + this->entries[i].entry_name_string_offset], BASE_ENTRY_STRING_SIZE);
-        printf("Name: %s - String Offset: 0x%.4X - Entry Offset: 0x%.4X\n", name, this->entries[i].entry_name_string_offset, this->entries[i].entry_offset);
+
+        out_size = snprintf(out_buf, 0x100, "Name: %s - String Offset: 0x%.4X - Entry Offset: 0x%.4X\n", name, this->entries[i].entry_name_string_offset, this->entries[i].entry_offset);
+        log_file.write(out_buf, out_size);
+
         delete name;
     }
-    printf("entries complete\n");
+    log_file << "entries complete\n";
 
     // skip over the size of the entry name string
     curr_file_pos += this->entry_name_string_length;
 
     u64 addendum_name_string_pos = curr_file_pos + (RKV2FILEPATHADDENDUM_SIZE * this->filepath_addendum_count);
 
-    printf("entering addendum cycle with curr_file_pos: 0x%.8X - addendum_name_string_pos: 0x%.8X\n", curr_file_pos, addendum_name_string_pos);
+    out_size = snprintf(out_buf, 0x100, "entering addendum cycle with curr_file_pos: 0x%.8X - addendum_name_string_pos: 0x%.8X\n", curr_file_pos, addendum_name_string_pos);
+    log_file.write(out_buf, out_size);
+
     // now read in the addendums
     for (u32 j = 0; j < this->filepath_addendum_count; j++) {
+        out_size = snprintf(out_buf, 0x100, "pre-entry at 0x%.8X\n", curr_file_pos);
+        log_file.write(out_buf, out_size);
+
         this->addendums[j].filepath_addendum_string_offset = read_64LE(&this->data[curr_file_pos]);
         this->addendums[j].timestamp = read_32LE(&this->data[curr_file_pos + 0x8]);
         this->addendums[j].entry_name_string_offset = read_32LE(&this->data[curr_file_pos + 0xC]);
+
+        out_size = snprintf(out_buf, 0x100, "addendum with details - str-off: 0x%.8X - ts: 0x%.8X - entry-str-off: 0x%.8X\n", this->addendums[j].filepath_addendum_string_offset,
+                            this->addendums[j].timestamp, this->addendums[j].entry_name_string_offset);
+        log_file.write(out_buf, out_size);
 
         curr_file_pos += RKV2FILEPATHADDENDUM_SIZE;
 
         u8* filepath_name = read_dynamic_string(&this->data[addendum_name_string_pos + this->addendums[j].filepath_addendum_string_offset], BASE_FILEPATHADDENDUM_STRING_SIZE);
         u8* name = read_dynamic_string(&this->data[name_string_pos + this->addendums[j].entry_name_string_offset], BASE_ENTRY_STRING_SIZE);
-        printf("Addendum Name: %s - String Offset: 0x%.8X - Linked Name: %s\n", filepath_name, this->addendums[j].filepath_addendum_string_offset, name);
+        out_size = snprintf(out_buf, 0x100, "Addendum Name: %s - String Offset: 0x%.8X - Linked Name: %s\n", filepath_name, this->addendums[j].filepath_addendum_string_offset, name);
+        log_file.write(out_buf, out_size);
+
         delete filepath_name;
         delete name;
     }
-    printf("addendums complete\n");
+    log_file << "addendums complete\n";
 
-    printf("all good :)\n");
+    log_file << "all good :)\n";
+
+    log_file.close();
+
     return true;
 }
 
